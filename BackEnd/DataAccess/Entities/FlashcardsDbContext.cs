fe74f5c3ee0a2f1808271984c48f06c6edab4b4c
Artur <arturgevorgyan87@yahoo.com>
2023-04-18 20:18:08 -0700

    First commit

---
+++ BackEnd/DataAccess/Entities/FlashcardsDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Entities;
+
+public partial class FlashcardsDbContext : DbContext
+{
+    public FlashcardsDbContext(DbContextOptions<FlashcardsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Flashcard> Flashcards { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Flashcard>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__FLASHCAR__3214EC2702CA630D");
+
+            entity.ToTable("FLASHCARDS");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Answer)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .HasColumnName("ANSWER");
+            entity.Property(e => e.Question)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .HasColumnName("QUESTION");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
